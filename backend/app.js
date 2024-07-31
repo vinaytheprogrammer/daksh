ae957948af0edb07f69478874229d66f3067c94c
vinaygupta.nitjsr <2022pgcsca008@nitjsr.ac.in>
2024-07-31 23:37:14 +0530

    added multiline comments for better understanding

--- backend/app.js
+++ backend/app.js
@@ -26,3 +26,5 @@ app.use('/api/auth', require('./routes/auth'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+// hello this is little test for changing commits
